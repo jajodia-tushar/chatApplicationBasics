34699b8a8a5ef5c9abeea66b60d1ec3e2d9896c1
Jajodia <tjajodia@internal.tavisca.com>
2019-07-30 15:37:10 +0530

    added Thread to send message every 10 seconds

--- src/main/java/com/tavisca/chat/ChatWebSocket.java
+++ src/main/java/com/tavisca/chat/ChatWebSocket.java
@@ -10,14 +10,20 @@ import java.io.IOException;
 @ServerEndpoint("/endpoint")
 public class ChatWebSocket {
 
+    static Thread thread;
     private ChatHelper chatHelper;
 
         public ChatWebSocket(){
             this.chatHelper = new ChatHelper();
+
         }
 
         @OnOpen
         public void onOpen(Session session){
+            if(this.thread == null) {
+                this.thread = new Thread(new MyRunnable());
+                this.thread.start();
+            }
             System.out.println("Open Connection: "+session.getId());
             ChatHelper.sessionMap.put(session.getId(),session);
         }
@@ -33,7 +39,25 @@ public class ChatWebSocket {
             System.out.println("Received Message from "+session.getId()+"  Message is "+ message);
             ChatHelper.sendMessage(message,session.getId());
         }
-
-
-
 }
+
+class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        while (true) {
+            for (Session session : ChatHelper.sessionMap.values()) {
+                try {
+                    session.getBasicRemote().sendText("-----10 Seconds Over-----");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
