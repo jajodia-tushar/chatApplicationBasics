06e37aaf14bb1a056369f699c32accabea506e14
Jajodia <tjajodia@internal.tavisca.com>
2019-07-30 15:10:10 +0530

    Two way communication of two clients and one server

---
+++ src/main/java/com/tavisca/chat/ChatHelper.java
@@ -0,0 +1,32 @@
+package com.tavisca.chat;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import javax.websocket.Session;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ChatHelper {
+
+    public static HashMap<String, Session> sessionMap =
+            new HashMap<>();
+
+    public static void sendMessage(String message, String senderId){
+        Pattern pattern = Pattern.compile("@(.*):(.*)");
+        Matcher matcher = pattern.matcher(message);
+        if(matcher.find()){
+            String receiverID = matcher.group(1);
+            String messageToBeSent = matcher.group(2);
+            Session session = sessionMap.get(receiverID);
+            try {
+                session.getBasicRemote().sendText("This message is from "+senderId+" and the message is -> "+messageToBeSent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
--- src/main/java/com/tavisca/chat/ChatWebSocket.java
+++ src/main/java/com/tavisca/chat/ChatWebSocket.java
@@ -10,25 +10,28 @@ import java.io.IOException;
 @ServerEndpoint("/endpoint")
 public class ChatWebSocket {
 
+    private ChatHelper chatHelper;
+
+        public ChatWebSocket(){
+            this.chatHelper = new ChatHelper();
+        }
+
         @OnOpen
         public void onOpen(Session session){
             System.out.println("Open Connection: "+session.getId());
+            ChatHelper.sessionMap.put(session.getId(),session);
         }
 
         @OnClose
         public void onClose(Session session){
             System.out.println("Connection Closed: "+session.getId());
+            ChatHelper.sessionMap.remove(session.getId());
         }
 
         @OnMessage
         public void onMessage(String message,Session session){
             System.out.println("Received Message from "+session.getId()+"  Message is "+ message);
-
-            try {
-                session.getBasicRemote().sendText(message.toUpperCase());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            ChatHelper.sendMessage(message,session.getId());
         }
 
 
